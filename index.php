bbedce2d2de606cfd8db9e2a2b4811a35a1f0af6
Oxversion1.5 <33206022+GideonAgboba@users.noreply.github.com>
2018-03-10 20:38:46 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+</head>
+<body>
+<p><a href="reg_island.php"></a>ISLAND</p>
+<p><a href="reg_mainland_1.php"></a>MAINLAND1</p>
+<p><a href="reg_mainland_2.php"></a>MAINLAND2</p>
+</body>
+</html>
